9def15bbdbdd730d3dcf0c6d13ffc62b3d4bc1d8
njogued <njogued@gmail.com>
2022-09-09 15:59:35 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlin.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char casesma
+
+	for (casesma = 'a'; casesma <= 'z'; casesma++)
+		putchar(casesma);
+		putchar('\n');
+	return (0);
+}
