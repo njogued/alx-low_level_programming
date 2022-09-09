19ccf245c2c74aba6954bee1216a8e18474bfed0
njogued <njogued@gmail.com>
2022-09-08 17:45:27 -0700

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main-entrypoint
  *
- * return:Always (0) success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -17,4 +17,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return(0);
 }
