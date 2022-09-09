5cc3d0a65f135b9906ad1d51ef0c9aadbae31839
njogued <njogued@gmail.com>
2022-09-09 16:19:31 -0700

    hIHG AND Low

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+	char high;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	putchar('\n');
+	for (high = 'A'; high <= 'Z'; high++)
+	putchar(high);
+	putchar(\n);
+ 
+	return (0);
+}	
