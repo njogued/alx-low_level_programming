d66a8126fe638b61dcdc2ade6e285df339c28a69
njogued <njogued@gmail.com>
2022-09-09 18:07:26 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	int base16;
 	char atof;
 
-	for (base16 = 0; base16 < 10; base16 16++)
+	for (base16 = 0; base16 < 10; base16++)
 		putchar(base16 + '0');
 	for (atof = 'a'; atof < 'g'; atof++)
 		putchar(atof);
