7cda0acae40dbf7198d03e9571ff4fa535e61fa4
njogued <njogued@gmail.com>
2022-09-09 18:23:24 -0700

    combi

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int combi;
+
+	for (combi = '0'; combi <= '9'; combi++)
+	{
+		putchar(combi);
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
