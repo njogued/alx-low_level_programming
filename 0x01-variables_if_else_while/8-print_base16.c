81bb0db217787b9dc25b96642b13687be1627531
njogued <njogued@gmail.com>
2022-09-09 18:03:33 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int base16;
+	char atof;
+
+	for (base16 = 0; base16 < 10; base 16++)
+		putchar(base16 + '0');
+	for (atof = 'a'; atof < 'g'; atof++)
+		putchar(atof);
+	return (0);
+}
+
