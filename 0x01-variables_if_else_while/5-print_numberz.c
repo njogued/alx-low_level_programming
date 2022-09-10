5f570775e262ba2598ace709081445717194e8b5
njogued <njogued@gmail.com>
2022-09-09 17:17:25 -0700

    putchar numberzz

---
+++ 0x01-variables_if_else_while/5-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numb2;
+
+	for (numb2 = 0; numb2 < 10; numb2++)
+		putchar("%d", numb2);
+	putchar("\n");
+	return (0);
+}
