e32d850af715466a3055669046312d28109ac77c
njogued <njogued@gmail.com>
2022-09-12 16:49:05 -0700

    Last commit

---
+++ 0x01-variables_if_else_while/2print.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0  (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
