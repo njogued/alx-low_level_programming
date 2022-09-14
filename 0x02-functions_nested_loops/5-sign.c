e6464864f1b2e2290a8fddd062a557e680c4c3d2
njogued <njogued@gmail.com>
2022-09-14 16:04:41 -0700

    Sign of the times

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign -prints the sign of an integer
+ * @n: integer
+ * Return: 1 if >0, 0 if 0, and -1 if <0
+ */
+int print_sign(int n)
+{
+	int ret = 0;
+
+	if(n > 0)
+	{
+		_putchar('+');
+		ret = 1;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		ret = -1;
+	}
+	else
+		_putchar ('0');
+	return (ret);
+}
+
