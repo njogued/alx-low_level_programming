95844d73bec382c5bf40afb71472af0dd905c172
njogued <njogued@gmail.com>
2022-09-14 16:46:26 -0700

    Last digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints last digit
+ * @lastlast: input number
+ * Return: The last digit
+ */
+int print_last_digit(int lastlast)
+{
+	int lst;
+	
+	if (lastlast > 0)
+	{
+		lst = lastlast % 10;
+	}
+	else
+	{
+		lastlast = lastlast * -1;
+		lst = lastlast % 10;
+	}
+	_putchar(lst + '0');
+	return (lst);
+}
