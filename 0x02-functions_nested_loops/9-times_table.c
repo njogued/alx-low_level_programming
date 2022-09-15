2bfa08a25b54b2b20b51a536bb08a2d1fc266021
njogued <njogued@gmail.com>
2022-09-14 19:27:38 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,43 +1,41 @@
 #include "main.h"
 /**
- *
- *  * print_times_table - Prints the times table of the input,
- *
- *   *                     starting with 0.
- *
- *    * @n: The value of the times table to be printed.
- *
- *     */
+ * print_times_table - Prints the times table
+ * @n: The value of the times table to be printed
+ * Return: 0
+ */
 
-void print_times_table(int n)
+void times_table(void)
 {
-	int num, mult, prod;
-	if (n >= 0 && n <= 15)
+	int i, j, tab;
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (num = 0; num <= n; num++)
+		for (j = 0; j <= 9; j++)
 		{
-			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
+			tab = i * j;
+			if ((tab / 10) == 0)
 			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(tab + '0');
+				if (j == 9)
+					continue;
 				_putchar(',');
 				_putchar(' ');
-				prod = num * mult;
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
 			}
-			_putchar('\n');
+			else
+			{
+				_putchar((tab / 10) + '0');
+				_putchar((tab % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
 }
+
+
