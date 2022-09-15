011b2f3f7252efa59fe4e9a1be92aedba4a28463
njogued <njogued@gmail.com>
2022-09-15 07:25:10 -0700

    0-9 without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - function to print numbers
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' && a != '4')
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
