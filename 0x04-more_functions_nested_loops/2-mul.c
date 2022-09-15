5d6df68077673136f55c29d3f71c56d5d98f7405
njogued <njogued@gmail.com>
2022-09-15 06:38:22 -0700

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function to print mutli of two
+ * @a: first integer input
+ * @b: second integer input
+ * Return: The multiple
+ */
+int mul(int a, int b)
+{
+	int reve;
+
+	reve = a * b;
+	return (reve);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,4 @@
 int _isdigit(int c);
 int _isupper(int c);
+int mul(int a, int b);
+int _putchar(char c)
