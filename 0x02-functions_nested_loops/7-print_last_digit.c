5e3a62a903376561e331644c8311ebce3316aabe
njogued <njogued@gmail.com>
2022-09-14 17:19:13 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,14 @@ int print_last_digit(int lastlast)
 	if (lastlast > 0)
 	{
 		lst = lastlast % 10;
+		_putchar(lst + '0')
 	}
 	else
 	{
 		lastlast = lastlast * -1;
 		lst = lastlast % 10;
+		_putchar(lst + '0');
+		lst = lst * -1;
 	}
-	_putchar(lst + '0');
 	return (lst);
 }
