fd34c38447486f8a61095e64c9f8ff356eeb0597
njogued <njogued@gmail.com>
2022-09-19 13:32:51 -0700

    Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,15 +4,17 @@
  * @s: string input
  * Return: 0
  */
-void print_rev(char *s)                                                               
-{                                                                             int i = 0;                                                                                                                                          
-	while (*(s + i) != '\0')                                                      
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
 	{
 		i++;
 	}
-         while (i--)                                                                   
-	{          
-		_putchar(s[i]);                                                       
-	}                                                                 
-	_putchar('\n');                                                                                   
-} 
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
