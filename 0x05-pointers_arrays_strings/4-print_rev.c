5bde4b8cc1a4f9a9333809c52d02414579987167
njogued <njogued@gmail.com>
2022-09-19 05:40:42 -0700

    Esrever using a cool for loop trick

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - Print the alphabet in reverse
+ * @s: string input
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int a, i;
+
+	a = 0;
+	for (i = 0; a < s[i]; i++)
+	{
+		i = i;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
