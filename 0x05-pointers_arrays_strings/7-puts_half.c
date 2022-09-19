ed1a85db92e8cd135edbdb06b7c900b158ebb234
njogued <njogued@gmail.com>
2022-09-19 07:10:05 -0700

    Half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,20 @@
 void puts_half(char *str)
 {
 	int a, i;
+
 	a = 0;
-	
 	for (i = 0; a < str[i]; i++)
 	{
 		i = i;
 	}
 	if (i % 2 == 0)
 	{
-		for (i = i / 2; a < str[i]; i++ )
+		for (i = i / 2; a < str[i]; i++)
 			_putchar(str[i]);
 	}
 	else
 	{
-		for (i = (i+1)/2; a < str[i]; i++)
+		for (i = (i + 1) / 2; a < str[i]; i++)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
