a5c21b1f617682897477901464c0024a86bdff4d
njogued <njogued@gmail.com>
2022-09-19 07:07:17 -0700

    Half puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - Prints second half
+ * @str: string to print
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int a, i;
+	a = 0;
+	
+	for (i = 0; a < str[i]; i++)
+	{
+		i = i;
+	}
+	if (i % 2 == 0)
+	{
+		for (i = i / 2; a < str[i]; i++ )
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (i+1)/2; a < str[i]; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
