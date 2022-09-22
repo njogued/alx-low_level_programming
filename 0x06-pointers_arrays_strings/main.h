99042962416756b32c36d4012cd3833c6f0de135
njogued <njogued@gmail.com>
2022-09-22 15:43:44 -0700

    Leet

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *s);
-char *leet(char *);
+char *leet(char *s);
 void print_number(int n);
 int _putchar(char c);
