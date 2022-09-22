d727be8248a5d70a5b86bca96ca44fe92b62e61b
njogued <njogued@gmail.com>
2022-09-22 14:45:22 -0700

    String in caps

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,21 +6,24 @@
  */
 char *cap_string(char *s)
 {
-	int i;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if (s[i] == 32 || s[i] == 46 || s[i] == 9 ||
-				s[i] == 10 || s[i] == 44 || s[i] == 59 ||
-				s[i] == 33 || s[i] == 63 || s[i] == 34 ||
-				s[i] == 40 || s[i] == 41 || s[i] == 123 ||
-				s[i] == 125)
+		for (i = 0; i < 13; i++)
 		{
-			if (s[i+1] <= 122 && s[i+1] >= 97)
+			if (*(s + count) == sep_words[i])
 			{
-				s[i+1] -= 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
+		count++;
 	}
 	return (s);
 }
