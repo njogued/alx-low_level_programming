5796913e04833d1229ba008a4648721756294066
njogued <njogued@gmail.com>
2022-09-22 15:48:05 -0700

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,25 +6,18 @@
  */
 char *leet(char *s)
 {
+	int i = 0, j;
+	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	while (s[i])
 	{
-		int count = 0, i;
-		int low_letters[] = {97, 101, 111, 116, 108};
-		int upp_letters[] = {65, 69, 79, 84, 76};
-		int numbers[] = {52, 51, 48, 55, 49};
+		for (j = 0; j < 10; j++)
+			if (s[i] == str[j])
+				s[i] = subs[j];
 
-		while (*(s + count) != '\0')
-		{
-			for (i = 0; i < 5; i++)
-			{
-				if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
-				{
-					*(s + count) = numbers[i];
-					break;
-				}
-			}
-			count++;
-		}
-
-		return (s);
+		i++;
 	}
+
+	return (s);
 }
