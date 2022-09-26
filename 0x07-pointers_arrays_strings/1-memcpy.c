7f970e1601ef4ef3bddba1ab9ccfc6df8084ce2b
njogued <njogued@gmail.com>
2022-09-26 12:28:10 -0700

    Memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - Function that copies bytes
+ * @n: bytes to be copied to string
+ * @dest: destination mem area
+ * @src: source string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+	unsigned int b;
+
+	a = 0;
+	b = 0;
+	while (a <=10 n)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
+}
