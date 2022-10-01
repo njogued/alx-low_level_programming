91c72aa32e866741b6b792d6f2dfce87f73f69bf
njogued <njogued@gmail.com>
2022-09-30 17:25:45 -0700

    New name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Function
+ * @argv - Argument vector
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
