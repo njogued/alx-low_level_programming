6ebcf5a2f133d04c257bde9ce06c22340219ba41
njogued <njogued@gmail.com>
2022-09-30 18:21:11 -0700

    New lines after argc,

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function main
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Zero
+ */
+int main(int argc, char *argv[])
+{
+	int count = 1;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}	
+	return (0);
+}
