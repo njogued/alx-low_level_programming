3e60be5b1c8dba8872e018c5ed5e9a16a10f391d
njogued <njogued@gmail.com>
2022-10-04 13:54:59 -0700

    str concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * str_concat - String concatenation
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to space in memory
+ */
+char *str_concat(char *s1, char *s2)
+{
+}
