fa7d73b20f69b41f653fab7eedad1d5d461b0899
njogued <njogued@gmail.com>
2022-10-06 08:31:33 -0700

    Nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,12 +6,13 @@
  * string_nconcat - Function to allocate memory and concat string
  * @s1: String one aka destination string
  * @s2: string two aka source string
+ * @n: Number of bytes
  * Return: pointer to the string s1
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *finalstr;
-	unsigned int i, len3, len1, len2;
+	unsigned int i, len1, len2, len3;
 
 	if (s1 == NULL)
 	{
@@ -23,21 +24,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-	if(n >= len2)
+	if (n >= len2)
 	{
 		n = len2;
 	}
-	len3 = len1 + n + 1;
-	finalstr = (char *)malloc(sizeof(*s1)*len3);
-	if (finalstr = NULL)
+	len3 = strlen(s1) + n + 1;
+	finalstr = malloc(sizeof(*finalstr) * len3);
+	if (finalstr == NULL)
 	{
 		return (NULL);
 	}
-	for(i = 0; i <= n; i++)
+	i = 0;
+	while (i < len1)
 	{
-		s1[len1] = s2[i];
-		len1++;
+		finalstr[i] = s1[i];
+		i++;
 	}
-	s1[len3] = '\0';
+	i = 0;
+	while (i < n)
+	{
+		finalstr[len1] = s2[i];
+		len1++;
+		i++;
+	}
+	finalstr[len3] = '\0';
 	return (finalstr);
 }
