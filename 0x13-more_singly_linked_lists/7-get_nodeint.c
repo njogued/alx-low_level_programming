cd6f2e171125788887fb287965e2a9e08f5575c8
njogued <njogued@gmail.com>
2022-10-28 06:15:18 -0700

    getnodeindex

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - Function to get nth node
+ * @head: entry point
+ * @index: nth index
+ * Return: nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		temp = temp->next;
+		count++;
+		if (count == index)
+			break;
+	}
+	if (count < index)
+	{
+		return (NULL);
+	}
+	return (temp);
+}
