f0a87e4947c3455a90ec92e015c2be6135e5d294
njogued <njogued@gmail.com>
2022-10-28 15:02:33 -0700

    checker

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,6 +16,8 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+size_t free_listint_safe(listint_t **h);
+size_t print_listint_safe(const listint_t *head);
 size_t print_listint(const listint_t *h);
 listint_t *find_listint_loop(listint_t *head);
 listint_t *reverse_listint(listint_t **head);
@@ -31,4 +33,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif /*lists*/
+
 
