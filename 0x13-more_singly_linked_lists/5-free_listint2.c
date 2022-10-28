f1de11f41f8a7e2a719113361b8259493b57f956
njogued <njogued@gmail.com>
2022-10-28 04:54:59 -0700

    Q5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - Function to free
+ * @head: entry point
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
+
