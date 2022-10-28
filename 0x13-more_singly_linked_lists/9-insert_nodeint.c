bceb2eeac73123a8b8c5ec0e40c067914596ecc4
njogued <njogued@gmail.com>
2022-10-28 07:44:32 -0700

    index9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,6 +25,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (temp && iterator < (idx - 1))
 	{
+		if (temp == NULL || temp->next == NULL)
+			return (NULL);
 		temp = temp->next;
 		iterator++;
 	}
