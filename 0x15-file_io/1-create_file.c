5c41790025dc41bd546c68348bbbd0103207e262
njogued <njogued@gmail.com>
2022-11-02 16:26:01 -0700

    CREATEFILE

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+int create_file(const char *filename, char *text_content)  
+{                                                          
+	int fd, file;                                      
+	size_t len;                                        
+
+
+	if (!filename)                                     
+		return (-1);                               
+
+	if (!text_content)                                 
+		text_content = "";                         
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S
+			_IRUSR | S_IWUSR);                                         
+
+	if (fd < 0)                                        
+		return (-1);                               
+
+	len = strlen(text_content);                        
+
+	file = write(fd, text_content, len);               
+
+	close(fd);                                         
+	return ((file < 0) ? (-1) : 1);                    
+} 
