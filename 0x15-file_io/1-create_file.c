31d5fdc0e8ef41e14df6931bda2886f79411a4fe
njogued <njogued@gmail.com>
2022-11-02 16:37:29 -0700

    createfilebetty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,27 +5,27 @@
  * @text_content: text content
  * Return: 1 on success and -1 if if failed
  */
-int create_file(const char *filename, char *text_content)  
-{                                                          
-	int fd, file;                                      
-	size_t len;                                        
+int create_file(const char *filename, char *text_content)
+{
+	int fd, file;
+	size_t len;
 
 
-	if (!filename)                                     
-		return (-1);                               
+	if (!filename)
+		return (-1);
 
-	if (!text_content)                                 
-		text_content = "";                         
+	if (!text_content)
+		text_content = "";
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);                                         
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (fd < 0)                                        
-		return (-1);                               
+	if (fd < 0)
+		return (-1);
 
-	len = strlen(text_content);                        
+	len = strlen(text_content);
 
-	file = write(fd, text_content, len);               
+	file = write(fd, text_content, len);
 
-	close(fd);                                         
-	return ((file < 0) ? (-1) : 1);                    
-} 
+	close(fd);
+	return ((file < 0) ? (-1) : 1);
+}
