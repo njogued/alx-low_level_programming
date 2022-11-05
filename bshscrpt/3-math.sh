3f6ceda7bf7adc8c9d9f3764417ce4cb2d3d75ae
njogued <njogued@gmail.com>
2022-11-05 04:08:30 -0700

    Agemaths

--- bshscrpt/3-math.sh
+++ bshscrpt/3-math.sh
@@ -3,9 +3,34 @@
 echo "User: $username"
 echo "Enter your age:"
 read age
+sleep 2
+a=$(($age+(2050-2022)))
+echo "Your age in 2050 will be:"
+echo "Calculating"
+echo "*...."
 sleep 1
-a=(($age))
+echo "**..."
+sleep 1
+echo "***.."
+sleep 1
+echo "****."
+sleep 1
+echo "*****"
+sleep 1
 echo "Your age in 2050 will be: $a"
-sleep 1
 math=$((($RANDOM%10)))
-echo "the random single integer is: $math"
+sleep 1
+echo "Your networth will be: $(($math*2)) million"
+let half=$math/2
+sleep 1
+echo "A random integer just because: "
+expr $half
+sleep 2
+echo "Exiting."
+sleep 1
+echo "Exiting.."
+sleep 1
+echo "Exiting..."
+sleep 1
+echo "Bye"
+sleep 1
