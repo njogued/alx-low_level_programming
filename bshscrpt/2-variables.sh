c336a9938fcdb50860bbc47dea7816d72d9d07c4
njogued <njogued@gmail.com>
2022-11-05 04:15:37 -0700

    varupdate

--- bshscrpt/2-variables.sh
+++ bshscrpt/2-variables.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-date=$(date)
+today=$(date)
 where=$(pwd)
 files=$(ls)
 name=$1
 age=$2
 sleep 1
-echo "$date" >> file.txt
+echo "$today" >> file.txt
 echo "These are the files in the directory, they will also be stored in file.txt"
 sleep 2
 echo "$files"
 echo "$files" >> file.txt
 sleep 2
-echo "Your name is $name and you are $age years old. You are in the $where on the date $date"
+echo "Your name is $name and you are $age years old. You are in the $where on the date $today"
+sleep 1
--- bshscrpt/README.md
+++ bshscrpt/README.md
@@ -1,4 +1,4 @@
-This is a bash practice repo. The bash scripts are based on the Youtube Playlist: https://www.youtube.com/playlist?list=PLIhvC56v63IKioClkSNDjW7iz-6TFvLwS by NetworkChuck
+This is a bash practice directory. The bash scripts are based on the Youtube Playlist: https://www.youtube.com/playlist?list=PLIhvC56v63IKioClkSNDjW7iz-6TFvLwS by NetworkChuck
 
 To run these scripts, use either bash filename.sh or chmod to make them executable
 
