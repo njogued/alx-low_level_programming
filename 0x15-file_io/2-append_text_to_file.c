68d6ac13a877090ff9326871c9b8f93bf87ca20b
njogued <njogued@gmail.com>
2022-11-05 01:48:52 -0700

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * append_text_to_file - Functions to append text to file
+ * @filename: a filename
+ * @text_content: content to add to file
+ * Return: 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
