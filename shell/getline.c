00254652ff5cc5e81d3d915805e09ae7d76ad11d
njogued <njogued@gmail.com>
2022-11-09 12:45:12 -0800

    Gtl

--- shell/getline.c
+++ shell/getline.c
@@ -4,18 +4,19 @@
 
 int main(void)
 {
-	size_t gtl = 1024;
+	size_t gtl = 1;
 	int n = 1;
-	char *ptr = malloc(1024);
+	char *ptr = malloc(1);
 	char *ave[] = {"/bin/ls", "/usr", NULL};
 
-	while (n)
+	while (1)
 	{
 		printf("$ ");
 		n = getline(&ptr, &gtl, stdin);
 		if (n != -1)
 		{
 			printf("%s\n", ptr);
+			free(ptr);
 			execve(ave[0], ave, NULL);
 		}
 		if (n == -1)
