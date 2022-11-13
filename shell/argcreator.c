58ef0d0ad933d6e0d991f041b2c4165c9c4bb2e3
njogued <njogued@gmail.com>
2022-11-13 04:00:28 -0800

    debug

--- shell/argcreator.c
+++ shell/argcreator.c
@@ -1,15 +1,37 @@
 #include "shell.h"
-char **argcreator(char *buffer)
+/*char **argcreator(char *buffer)
 {
 	int i = 0;
 	char *tok, **args;
-	tok = strtok(buffer, " ")
+	tok = strtok(buffer, " ");
 	while (tok)
 	{
 		args[i] = tok;
 		i++;
 		tok = (NULL, " ");
 	}
-	printf("%s\n", args);
 	return (args);
+} */
+int main(void)
+{
+	char *tok, *line, *split_args[3];
+	char *line1 = "ls -a -l";
+	int i = 0, u;
+	line =  malloc(sizeof(char) * 10);
+	strcpy(line, line1);
+        tok = strtok(line, " ");
+        while (tok)
+        {
+                split_args[i] = tok;
+                i++;
+                tok = strtok(NULL, " ");
+        }
+	printf("OK SO FAR\n");
+
+	/*char **split_args = argcreator(line);*/
+	for(u = 0; u < i; u++)
+	{
+		printf("%s\n", split_args[u]);
+	}
+	return (0);
 }
--- shell/shell.h
+++ shell/shell.h
@@ -1,6 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
