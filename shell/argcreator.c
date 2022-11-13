009e0a443e4dfda2239c3574f461f9c185eada98
njogued <njogued@gmail.com>
2022-11-13 06:15:19 -0800

    argcreator

--- shell/argcreator.c
+++ shell/argcreator.c
@@ -14,10 +14,19 @@
 } */
 int main(void)
 {
-	char *tok, *line, *split_args[3];
-	char *line1 = "ls -a -l";
+	char *tok, *line, **split_args;
+	char *line1 = "/bin/ls -a -l";
 	int i = 0, u;
+	split_args = malloc(sizeof(strlen(line)));
+	if (split_args == 0)
+	{
+		perror("Failed to allocate");
+	}
 	line =  malloc(sizeof(char) * 10);
+	if (line == 0)
+	{
+		perror("Failed to allocate memory");
+	}
 	strcpy(line, line1);
         tok = strtok(line, " ");
         while (tok)
@@ -26,12 +35,20 @@ int main(void)
                 i++;
                 tok = strtok(NULL, " ");
         }
+	split_args[i] = NULL;
+	printf("%d\n", i);
+
 	printf("OK SO FAR\n");
 
 	/*char **split_args = argcreator(line);*/
 	for(u = 0; u < i; u++)
 	{
-		printf("%s\n", split_args[u]);
+		printf("args[%d]: %s\n", u, split_args[u]);
+	}
+	if(execve(split_args[0], split_args, NULL) == -1)
+	{
+		perror("Failed to execute command");
+		return(-1);
 	}
 	return (0);
 }
