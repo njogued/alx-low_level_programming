01845f7fb91249600062207baf2d2cceb3ec4950
njogued <njogued@gmail.com>
2022-11-18 13:45:42 -0800

    R

--- 0x01-variables_if_else_while/printcomb.c
+++ 0x01-variables_if_else_while/printcomb.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 int main()
 {
-	int a, b;
-	for(a = 0; a < 10; a++)
-	{
-	for(b = 0; b < 10; b++)
-	{
-	if (a < b && a != b)
-	{
-	putchar(a + '0');
-	putchar(b + '0');
-	putchar(',');
-	}
-	}
-	}
-	return (0);
-	}
-
+    int a, b, c;
+    for(a = 0; a < 10; a++)
+    {
+        for(b = 0; b < 10; b++)
+        {
+            for(c = 0; c < 10; c++)
+            {
+                if(a < b && b < c && a != b && a != c && b != c)
+                {
+                    putchar(a + '0');
+                    putchar(b + '0');
+                    putchar(c + '0');
+                    if (a == 7 &&b ==8 &&c ==  9)
+                        continue;
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+    return(0);
+}
+                    
