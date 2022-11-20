31c9a88b0402df24a0a5e9de45ec7fde2c3433c4
njogued <njogued@gmail.com>
2022-11-20 02:05:23 -0800

    LesPrimes

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,28 +5,28 @@
  */
 int main(void)
 {
-        long int factors, itr = 1, prime = 0, nput = 612852475143;
+	unsigned long int factors, itr = 1, prime = 0, nput = 612852475143;
 	int facno = 0;
 
-        for (factors = 2; factors < nput; factors++)
-        {
-                if ((nput % factors) == 0)
-                {
-                itr = 1;
-                facno = 0;
-                while (itr < factors)
-                {
-                        if ((factors % itr) == 0)
-                        {
-                                facno++;
-                        }
-                        itr++;
+	for (factors = 2; factors < nput; factors++)
+	{
+		if ((nput % factors) == 0)
+		{
+			itr = 1;
+			facno = 0;
+			while (itr < factors)
+			{
+				if ((factors % itr) == 0)
+				{
+					facno++;
+				}
+				itr++;
+			}
+			if (facno == 1)
+				prime = factors;
 		}
-                if (facno == 1)
-                        prime = factors;
-                }
 
-        }
-        printf("%ld\n", prime);
-        return (0);
+	}
+	printf("%ld\n", prime);
+	return (0);
 }
