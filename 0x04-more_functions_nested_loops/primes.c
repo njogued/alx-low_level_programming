fbcde25fafd5915eb16db59e40871ccf3f84fe55
njogued <njogued@gmail.com>
2022-11-20 04:38:12 -0800

    Prime numbers with struct [advanced]

--- 0x04-more_functions_nested_loops/primes.c
+++ 0x04-more_functions_nested_loops/primes.c
@@ -1,32 +1,112 @@
+#include <stdlib.h>
 #include <stdio.h>
+int check_prime(int prime);
+/**
+ * Program that takes in user input and displays the prime numbers
+ */
+
+typedef struct nos List_Nos;
+/**
+ * struct nos - structure to store prime numbers
+ * @numz: prime number stored
+ * @node: link to the next node
+ */
+struct nos{
+        int numz;
+        List_Nos *node;
+};
+/**
+ * add_node - Function to add node to the start of a list
+ * @head: head node
+ * @num: number to add to start of list
+ * Return: list
+ */
+List_Nos *add_node(List_Nos **head, int num)
+{
+	List_Nos *new;
+	new = malloc(sizeof(List_Nos));
+	if (new == NULL)
+	{
+		perror("Failed to allocate");
+	}
+	new->numz = num;
+	if ((*head) == NULL)
+	{
+		new->node = NULL;
+		(*head) = new;
+	}
+	else
+	{
+		new->node = (*head);
+		(*head) = new;
+	}
+	return (*head);
+}
+/**
+ * print_nos - Function to print the prime numbers
+ * @head: head node
+ */
+void print_nos(List_Nos *head)
+{
+	int i = 1, n;
+	while (head)
+	{
+		n = check_prime(head->numz);
+		if (n == 1)
+		{
+			printf("Prime number[%d]: %d\n",i, head->numz);
+		i++;
+		}
+		head = head->node;
+	}
+}
+/**
+ * check_prime - check if a number is prime
+ * @prime: number to check
+ * Return: 1 if number is prime, 0 if it is not
+ */
+int check_prime(int prime)
+{
+	int iterator, facno = 0, n = 0;
+	for (iterator = 1; iterator < prime; iterator++)
+	{
+		if (prime % iterator == 0)
+			facno++;
+	}
+	if (facno == 1)
+	{
+		n = 1;
+	}
+	return (n);
+	
+}
 /**
  * main - Entry point
- * Return: Always 0
+ * The main function takes input from user
+ * Passes the input to add_node which adds it to linked list
+ * Calls the print_nos function which prints the prime numbers
+ * Return: 0 on success
  */
 int main(void)
 {
-	unsigned long int factors, itr = 1, prime = 0, nput = 612852475143;
-	int facno = 0;
+	int factor, nput;
+	size_t bufsize = 0;
+	List_Nos *head;
+	char *buffer = NULL;
 
-	for (factors = 2; factors < nput; factors++)
+	getline(&buffer, &bufsize, stdin);
+	nput = atoi(buffer);
+	printf("The input is: %d\n", nput);
+
+	head = NULL;
+
+	for (factor = 2; factor < nput; factor++)
 	{
-		if ((nput % factors) == 0)
+		if ((nput % factor) == 0)
 		{
-			itr = 1;
-			facno = 0;
-			while (itr < factors)
-			{
-				if ((factors % itr) == 0)
-				{
-					facno++;
-				}
-				itr++;
-			}
-			if (facno == 1)
-				prime = factors;
+			add_node(&head, factor);
 		}
-
 	}
-	printf("%ld\n", prime);
+	print_nos(head);
 	return (0);
 }
