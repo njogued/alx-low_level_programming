875e2f71a0551d975a680070d6cdd507af86a29e
njogued <njogued@gmail.com>
2022-11-20 02:14:22 -0800

    gp

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
+#include <math.h>
+
 /**
- * main - Entry point
- * Return: Always 0
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long int factors, itr = 1, prime = 0, nput = 612852475143;
-	int facno = 0;
+	long int n;
+	long int max;
+	long int i;
 
-	for (factors = 2; factors < nput; factors++)
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
 	{
-		if ((nput % factors) == 0)
-		{
-			itr = 1;
-			facno = 0;
-			while (itr < factors)
-			{
-				if ((factors % itr) == 0)
-				{
-					facno++;
-				}
-				itr++;
-			}
-			if (facno == 1)
-				prime = factors;
-		}
-
+		max = 2;
+		n /= 2;
 	}
-	printf("%ld\n", prime);
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
 	return (0);
 }
---
+++ 0x04-more_functions_nested_loops/primes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int factors, itr = 1, prime = 0, nput = 612852475143;
+	int facno = 0;
+
+	for (factors = 2; factors < nput; factors++)
+	{
+		if ((nput % factors) == 0)
+		{
+			itr = 1;
+			facno = 0;
+			while (itr < factors)
+			{
+				if ((factors % itr) == 0)
+				{
+					facno++;
+				}
+				itr++;
+			}
+			if (facno == 1)
+				prime = factors;
+		}
+
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
