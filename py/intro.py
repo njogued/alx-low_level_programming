080731acc1bbc09d52d846c0ee43cdd01d0f9dd2
njogued <njogued@gmail.com>
2022-11-27 05:08:15 -0800

    Py

---
+++ py/intro.py
@@ -0,0 +1,10 @@
+#!/bin/python
+
+print("Hello world")
+a = 5
+print("Hello World", a)
+print("Hello World" * 5)
+print("The value of a is: ", a)
+names = ["Name1", "Name2", "Name3"]
+print(names)
+print(names[-2:])
