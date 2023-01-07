74aee7435b876441505de6fcb83818f1d53c14ce
njogued <njogued@gmail.com>
2023-01-07 05:13:30 -0800

    task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,8 +7,8 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
-	hash_table_s *table;
+	unsigned long int i = 0;
+	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
