a2b9f4646bf8f7ddc10467316293fc78144fa9fb
njogued <njogued@gmail.com>
2023-01-12 02:10:27 -0800

    Open hashing / Separate chaining implementation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (strcmp(key, temp->key) == 0)
 			{
 				free(ht->array[index]->value);
-				ht->array[index] = strdup(value);
+				ht->array[index]->value = strdup(value);
 				return (1);
 			}
 			temp = temp->next;
