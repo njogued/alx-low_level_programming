6059620513b2af9a3ba086bda4649b150478e815
njogued <njogued@gmail.com>
2023-01-12 03:24:05 -0800

    Push Task

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,16 +1,40 @@
-#include "hash_tables_h"
+#include "hash_tables.h"
 /**
  *
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int iterator = 0;
+	unsigned long int index = 0;
+	hash_node_t *temp = NULL;
+	int comma = 0;
 
 	if (!ht)
 	{
 		printf("{}");
 	}
 	printf("{");
-	for (iterator = 0; iterator < ht->size; iterator++)
+	for (index = 0; index < ht->size; index++)
 	{
-
+		if (ht->array[index] && ht->array[index]->next == NULL)
+		{
+			if (comma > 0)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[index]->key, (char *)\
+			ht->array[index]->value);
+			comma++;
+		}
+		else if (ht->array[index] && ht->array[index]->next && comma > 0)
+		{
+			if (comma > 0)
+				printf(", ");
+			temp = ht->array[index];
+			while (temp->next)
+			{
+				printf("'%s': '%s'", temp->key, (char *)temp->value);
+				temp = temp->next;
+			}
+			comma++;
+		}
+	}
+	printf("}\n");
+}
