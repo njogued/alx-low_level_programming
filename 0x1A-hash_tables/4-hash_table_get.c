14a1281c287566b657359245d4087dee7c4a9186
njogued <njogued@gmail.com>
2023-01-12 02:35:45 -0800

    Get element for value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves value for key
+ * @ht: hashtable checked
+ * @key: key for value to retrieve
+ * Return: Value on success or NULL if failed
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp = NULL;
+	unsigned long int index = 0;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (!ht || !key || ht->array[index] == NULL)
+		return (NULL);
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			return (temp->value);
+		}
+		temp = temp->next;
+	}
+	free(temp);
+	return (NULL);
+}
