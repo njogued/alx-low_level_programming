fa997ebd00cf6a75b47305fa5861e5c8d9c731a2
njogued <njogued@gmail.com>
2023-01-12 02:08:01 -0800

    Open hashing / Separate chaining implementation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,16 +9,16 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *item;
-	unsigned long int index;
+	hash_node_t *item, *temp = NULL;
+	unsigned long int index = 0;
 
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL || !key || !value || !ht)
 	{
 		return (0);
 	}
-	strcpy(item->key, key);
-	strcpy(item->value, value);
+	item->key = strdup(key);
+	item->value = strdup(value);
 	item->next = NULL;
 	index = key_index((const unsigned char *)key, ht->size);
 
@@ -28,6 +28,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
+		temp = ht->array[index];
+		while (temp)
+		{
+			if (strcmp(key, temp->key) == 0)
+			{
+				free(ht->array[index]->value);
+				ht->array[index] = strdup(value);
+				return (1);
+			}
+			temp = temp->next;
+		}
 		item->next = ht->array[index];
 		ht->array[index] = item;
 	}
