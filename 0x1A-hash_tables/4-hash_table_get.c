d5240896b5dbae4526f746f07b978f052a8b5b29
njogued <njogued@gmail.com>
2023-01-12 02:53:03 -0800

    Get element for value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
-		return (NULL)
+		return (NULL);
 	temp = ht->array[index];
 	while (temp)
 	{
