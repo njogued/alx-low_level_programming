6e0c0446f2a21ac496a140fe708cccb909081d1b
njogued <njogued@gmail.com>
2023-01-23 13:00:06 -0800

    QS

---
+++ sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+void swap(int *array, int index_1, int index_2)
+{
+	int temp;
+
+	temp = array[index_1];
+	array[index_1] = array[index_2];
+	array[index_2] = temp;
+}
+
+void quick_sort(int *array, size_t size)
+{
+	size_t pivot = 0;
+	size_t low = 0, high = 0;
+
+	pivot = size - 1; 
+
+	high = pivot - 1;
+	while(low < high)
+	{
+		if(array[low] < array[pivot])
+		{
+			low += 1;
+			printf("Pivot > Low");
+		}
+		if(array[low] > array[pivot])
+		{
+			if(array[high] > array[pivot])
+			{
+				high -= 1;
+			}
+			if(array[high] < array[pivot])
+			{
+				swap(array, low, high);
+			}
+		
+		printf("lOW: %ld\n", low);
+		}
+	}
+	if(low != pivot - 1)
+	{
+		swap(array, low, pivot);
+	}
+	print_array(array, size);
+
+}
